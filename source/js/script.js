c1795345c46c14c2b213ca03f8dbc8440422f18f
ele828 <dongjiemusic@qq.com>
2017-06-01 23:51:41 +0800

    tweak theme templates and styles

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,30 @@
-function init() {
-  $('body .hidden-prince').each(function () {
-    $(this).fadeIn(600).removeClass('hidden-prince');
+var quoteEndpoint = "https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+function initQuotes(count) {
+  // Avoid deep recursive
+  if (count > 10) {
+    $(".quotes").text("Make things that really matter.");
+    return;
+  }
+  // If in homepage, fetches quotes
+  $.getJSON(quoteEndpoint).then(r => {
+    var quotes = r.quoteText;
+    if (!quotes || quotes.length > 60 || quotes.length <= 0) {
+      initQuotes(++count);
+    } else {
+      $(".quotes").text(quotes);
+    }
   });
+}
+initQuotes(0);
 
+function init() {
   $('.post-block p img').each(function () {
     $(this).wrap('<a data-fancybox="images" href="' + this.src + '" data-caption=" ' + this.alt + '" class="fancybox"></a>');
-    $(this).attr('src', this.src+'?imageMogr2/thumbnail/300x300/gravity/North/crop/200x200');
+    $(this).attr('src', this.src + '?imageMogr2/thumbnail/300x300/gravity/North/crop/200x200');
   });
+
+  // Align all images to center
+  $('.post-entry p:has(a.fancybox)').css({ 'text-align': 'center' });
 
   if ($.fancybox) {
     $('.fancybox').fancybox({
